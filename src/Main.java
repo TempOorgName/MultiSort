4dbc5ebc2cd31f831283a5de8e8f7fef4e6b6cd4
Zaki Bushiri <bushirizaki@gmail.com>
2026-02-10 15:40:55 -0500

    del

--- src/Main.java
+++ src/Main.java
@@ -9,28 +9,7 @@ public class Main {
         int secondAns;
         boolean validInput = false;
 
-        System.out.println("== Welcome to MultiSort ==");
 
-        while(!validInput)
-        {
-            System.out.println("1. Sort numbers");
-            System.out.println("2. Sort words");
-            System.out.println("3. Sort sentence length");
-
-            try
-            {
-               firstAns = key.nextInt();
-               if(firstAns>0 && firstAns<4)
-                   validInput = true;
-               else
-                   System.out.println("Enter 1, 2, or 3");
-            }
-            catch(Exception e)
-            {
-                System.out.println("Invalid input detected enter a number.");
-                key.next();
-            }
-        }
 
 
 
