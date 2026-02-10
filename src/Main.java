71244a0220a7a2def8640cca84d8fad71c72712c
Xavier <XLMILLER@email.sc.edu>
2026-02-10 16:13:30 -0500

    Del

--- src/Main.java
+++ src/Main.java
@@ -17,22 +17,5 @@ public class Main {
             System.out.println("2. Sort words");
             System.out.println("3. Sort sentence length");
 
-            try
-            {
-               firstAns = key.nextInt();
-               if(firstAns>0 && firstAns<4)
-                   validInput = true;
-               else
-                   System.out.println("Enter 1, 2, or 3");
-            }
-            catch(Exception e)
-            {
-                System.out.println("Invalid input detected enter a number.");
-                key.next();
-            }
-        }
-
-
-
     }
 }
